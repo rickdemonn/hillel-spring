9c3e0dde0f93f0c12e91644b8cde3f0fd1f28a4f
java <rickdemonn@gmail.com>
2019-06-22 13:10:38 +0300

    try to add lombok

--- src/main/java/hillel/spring/Apple.java
+++ src/main/java/hillel/spring/Apple.java
@@ -1,44 +1,12 @@
 package hillel.spring;
 
 
-import java.util.Objects;
+import lombok.Data;
 
+import java.util.Objects;
+@Data
 public class Apple {
-    private final Integer weigth;
+    private final Integer weight;
     private final String color;
 
-    public Apple(Integer weigth, String color) {
-        this.weigth = weigth;
-        this.color = color;
-    }
-
-    public Integer getWeigth() {
-        return weigth;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Apple apple = (Apple) o;
-        return Objects.equals(weigth, apple.weigth) &&
-                Objects.equals(color, apple.color);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(weigth, color);
-    }
-
-    @Override
-    public String toString() {
-        return "Apple{" +
-                "weight=" + weigth +
-                ", color='" + color + '\'' +
-                '}';
-    }
 }
