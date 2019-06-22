20b92fdc22af5dab7f2c1e330176a7de36ada29e
java <rickdemonn@gmail.com>
2019-06-22 11:49:26 +0300

    first commit

---
+++ src/main/java/hillel/spring/Apple.java
@@ -0,0 +1,43 @@
+package hillel.spring;
+
+import java.util.Objects;
+
+public class Apple {
+    private final Integer weigth;
+    private final String color;
+
+    public Apple(Integer weigth, String color) {
+        this.weigth = weigth;
+        this.color = color;
+    }
+
+    public Integer getWeigth() {
+        return weigth;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Apple apple = (Apple) o;
+        return Objects.equals(weigth, apple.weigth) &&
+                Objects.equals(color, apple.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weigth, color);
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "weigth=" + weigth +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
