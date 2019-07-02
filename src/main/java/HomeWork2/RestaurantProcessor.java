2397e4597079ab186eea4e3ae3ddeea431d8585c
dmytro.denysko <dmytro.denysko@pivdenny.ua>
2019-07-02 15:24:40 +0300

    HW2

--- src/main/java/HomeWork2/RestaurantProcessor.java
+++ src/main/java/HomeWork2/RestaurantProcessor.java
@@ -1,12 +1,12 @@
 package HomeWork2;
 
-
-
 import java.util.*;
+
+import java.util.stream.Collectors;
 
 public class RestaurantProcessor {
 
-    public static void printNamesOfDish(Restaurant restaurant) throws Exception{
+    public static void printNamesOfDish(Restaurant restaurant) {
         /*for (Dish menu: restaurant.getMenu()) {
             System.out.println(menu.getName());
         }*/
@@ -16,8 +16,8 @@ public class RestaurantProcessor {
     }
 
 
-    public static Optional<Restaurant> pickLowCalories(Restaurant restaurant) throws Exception{
-        List<Dish> dishList = restaurant.getMenu();
+    public static Optional<Restaurant> pickLowCalories(Restaurant restaurant) {
+       /* List<Dish> dishList = restaurant.getMenu();
         List<Dish> newList = new ArrayList<>();
 
         for (Dish dish: dishList) {
@@ -30,11 +30,18 @@ public class RestaurantProcessor {
         }
 
         Restaurant newRes = new Restaurant(newList);
-        return Optional.of(newRes);
+        return Optional.of(newRes);*/
+
+
+        List<Dish> newDish = restaurant.getMenu().stream()
+                .filter(dish -> dish.getCalories() < 175)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        return Optional.of(new Restaurant(newDish));
     }
 
-    public static Restaurant pickTopCalories(Restaurant restaurant) throws Exception{
-        List<Dish> dishList = restaurant.getMenu();
+    public static Restaurant pickTopCalories(Restaurant restaurant) {
+       /* List<Dish> dishList = restaurant.getMenu();
 
         dishList.sort(getCompByCalories());
 
@@ -43,23 +50,41 @@ public class RestaurantProcessor {
         for(int i = 0; i < 3; i++){
             newList.add(dishList.get(i));
             System.out.println(newList.get(i).getName());
-        }
-        return new Restaurant(newList);
+        }*/
+
+        return new Restaurant(restaurant.getMenu().stream()
+                .sorted(getCompByCaloriesReverse())
+                .limit(3)
+                .peek(System.out::println)
+                .collect(Collectors.toList()));
+
+
     }
 
-    public static void printSortByTypeAndByName(Restaurant restaurant) throws Exception {
-        List<Dish> dishList = restaurant.getMenu();
+    public static void printSortByTypeAndByName(Restaurant restaurant) {
+        /*List<Dish> dishList = restaurant.getMenu();
 
         dishList.sort(getCompByType());
-        dishList.sort(getCompByName());
 
         for (Dish dish: dishList) {
             System.out.println(dish.getName());
         }
+
+        dishList.sort(getCompByName());
+
+        for (Dish dish: dishList) {
+            System.out.println(dish.getName());
+        }*/
+
+        restaurant.getMenu().stream()
+                .sorted(getCompByType())
+                .peek(System.out::println)
+                .sorted(getCompByName())
+                .forEach(System.out::println);
     }
 
     public static Map<DishType, Double> getAvgByType(Restaurant restaurant) {
-        List<Dish> dishList = restaurant.getMenu();
+        /*List<Dish> dishList = restaurant.getMenu();
         
         Map<DishType, Double> mapOfAvg = new HashMap<>();
 
@@ -83,12 +108,19 @@ public class RestaurantProcessor {
         for (Map.Entry<DishType, Double> entry : mapOfAvg.entrySet()) {
             System.out.println(entry.getKey()+" : "+entry.getValue());
         }
+        return mapOfAvg;*/
 
-        return mapOfAvg;
+        Map<DishType, Double> typeToAvg = restaurant.getMenu().stream()
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.averagingInt(Dish::getCalories)));
+
+        typeToAvg.entrySet().forEach(System.out::println);
+
+        return typeToAvg;
+
     }
 
     public static Map<DishType, List<Dish>> groupByType(Restaurant restaurant) {
-        Map<DishType, List<Dish>> typeToDishList = new HashMap<>();
+        /*Map<DishType, List<Dish>> typeToDishList = new HashMap<>();
         List<Dish> beef = new ArrayList<>();
         List<Dish> chicken = new ArrayList<>();
         List<Dish> vegetables = new ArrayList<>();
@@ -109,11 +141,19 @@ public class RestaurantProcessor {
         for (Map.Entry<DishType, List<Dish>> entry : typeToDishList.entrySet()) {
             System.out.println(entry.getKey()+" : "+entry.getValue().toString());
         }
+        return typeToDishList;*/
+
+
+        Map<DishType, List<Dish>> typeToDishList = restaurant.getMenu().stream()
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.toList()));
+
+        typeToDishList.entrySet().forEach(System.out::println);
+
         return typeToDishList;
     }
 
-    public static Map<DishType, List<String>> groupByBio(Restaurant restaurant) {
-        Map<DishType, List<String>> typeToName = new HashMap<>();
+    public static Map<DishType, List<String>> groupByBioToName(Restaurant restaurant) {
+        /*Map<DishType, List<String>> typeToName = new HashMap<>();
 
         List<String> beef = new ArrayList<>();
         List<String> chicken = new ArrayList<>();
@@ -136,22 +176,30 @@ public class RestaurantProcessor {
             System.out.println(entry.getKey()+" : "+entry.getValue().toString());
         }
 
+        return typeToName;*/
+
+        Map<DishType, List<String>> typeToName = restaurant.getMenu().stream()
+                .filter(Dish::getIsBio)
+                .collect(Collectors.groupingBy(Dish::getType, Collectors.mapping(Dish::getName, Collectors.toList())));
+
+        typeToName.entrySet().forEach(System.out::println);
+
         return typeToName;
     }
 
-    private static Comparator<Dish> getCompByCalories()
+    private static Comparator<Dish> getCompByCaloriesReverse()
     {
         return (d1, d2) -> d2.getCalories().compareTo(d1.getCalories());
     }
 
     private static Comparator<Dish> getCompByType()
     {
-        return (d1, d2) -> d1.getType().compareTo(d2.getType());
+        return Comparator.comparing(Dish::getType);
     }
 
     private static Comparator<Dish> getCompByName()
     {
-        return (d1, d2) -> d1.getName().compareTo(d2.getName());
+        return Comparator.comparing(Dish::getName);
     }
 
     private static double calculateAverage(List<Integer> marks) {
--- src/test/java/HomeWork2/RestaurantProcessorTest.java
+++ src/test/java/HomeWork2/RestaurantProcessorTest.java
@@ -2,17 +2,15 @@ package HomeWork2;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+
 
 import static org.assertj.core.api.Assertions.*;
 
 
 public class RestaurantProcessorTest {
 
-    List<Dish> dishList = Arrays.asList(
+    private List<Dish> dishList = Arrays.asList(
             new Dish("Cesar", 100, true, DishType.CHICKEN),
             new Dish("Meat", 200, true, DishType.BEEF),
             new Dish("Herb", 50, false, DishType.VEGETABLES),
@@ -21,7 +19,7 @@ public class RestaurantProcessorTest {
             new Dish("Steak", 250, true, DishType.BEEF),
             new Dish("Potato", 100, true, DishType.VEGETABLES)
     );
-    Restaurant restaurant = new Restaurant(dishList);
+    private Restaurant restaurant = new Restaurant(dishList);
 
     @Test
     public void printNamesOfDishTest() throws Exception{
@@ -33,15 +31,19 @@ public class RestaurantProcessorTest {
     @Test
     public void pickLowCaloriesTest() throws Exception{
         Optional<Restaurant> restaurant = RestaurantProcessor.pickLowCalories(this.restaurant);
+        if (restaurant.isPresent()) {
+            assertThat(restaurant.get().getMenu()).hasSize(4);
+        } else {
+            fail("Restaurant should be present");
+        }
 
-        assertThat(restaurant.get().getMenu()).hasSize(4);
     }
 
     @Test
     public void pickTopCaloriesTest() throws Exception{
         Restaurant restaurant = RestaurantProcessor.pickTopCalories(this.restaurant);
 
-        assertThat(restaurant.getMenu()).hasSize(3);
+        assertThat(restaurant.getMenu().get(1).getCalories()).isEqualTo(200);
     }
 
     @Test
@@ -52,14 +54,14 @@ public class RestaurantProcessorTest {
     }
 
     @Test
-    public void getAvgByTypeTest() throws Exception {
+    public void getAvgByTypeTest() {
         Map<DishType, Double> map = RestaurantProcessor.getAvgByType(restaurant);
 
         assertThat(map).hasSize(3);
     }
 
     @Test
-    public void groupByTypeTest() throws Exception {
+    public void groupByTypeTest() {
         Map<DishType, List<Dish>> map = RestaurantProcessor.groupByType(restaurant);
 
         assertThat(map).hasSize(3);
@@ -67,9 +69,14 @@ public class RestaurantProcessorTest {
     }
 
     @Test
-    public void groupByBioName() throws Exception {
-        Map<DishType, List<String>> map = RestaurantProcessor.groupByBio(restaurant);
+    public void groupByBioToNameTest() {
+        Map<DishType, List<String>> map = RestaurantProcessor.groupByBioToName(restaurant);
 
-        assertThat(map).hasSize(3);
+        Map<DishType, List<String>> mapToGo = new HashMap<>();
+        mapToGo.put(DishType.BEEF, Arrays.asList("Meat", "Steak"));
+        mapToGo.put(DishType.CHICKEN, Arrays.asList("Cesar", "ChickenRoll"));
+        mapToGo.put(DishType.VEGETABLES, Collections.singletonList( "Potato"));
+
+        assertThat(map).isEqualTo(mapToGo);
     }
 }
